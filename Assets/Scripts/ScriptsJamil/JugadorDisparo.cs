80ff495ca1708d5bc8dd39d6313def89a18cf21c
Jamil Cerrón <luis.cerron.2010@gmail.com>
2023-11-05 14:01:46 -0500

    Se actualizó el disparo a uno similar a los juegos topDown (la dirección de la mirada de Mike apunta a la posición del mouse) y se añadió cinemachine

--- Assets/Scripts/ScriptsJamil/JugadorDisparo.cs
+++ Assets/Scripts/ScriptsJamil/JugadorDisparo.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 public class JugadorDisparo : MonoBehaviour
 {
-
     public GameObject bulletPrefab;
     public GameObject Arma;
     public GameObject puntoDeDisparo;
-
     public Camera cámara;
-
     public Vector2 direction;
 
+    private Animator direcciónMirada;
 
     void Start()
     {
         cámara = Camera.main;
+        direcciónMirada = GetComponent<Animator>();
     }
+
     void Update()
     {
-        Disparo();
         Apuntar();
+        Disparo();
     }
 
     void Apuntar()
     {
         Vector3 mousePosition = cámara.ScreenToWorldPoint(Input.mousePosition);
-
         direction = mousePosition - Arma.transform.position;
-
         Arma.transform.up = direction.normalized;
+
+        float ángulo = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //Este devuelve un valor de -180 a 180
+        
+        if (ángulo < 0)
+        {
+            ángulo += 360;
+
+            if (ángulo >= 315)
+            {
+                ángulo = 0;
+            }
+        }
+        //Debug.Log(ángulo);
+
+        direcciónMirada.SetFloat("Ángulo", ángulo);
     }
 
     void Disparo()
@@ -40,7 +54,6 @@ public class JugadorDisparo : MonoBehaviour
             GameObject obj = Instantiate(bulletPrefab);
             obj.transform.position = puntoDeDisparo.transform.position;
             obj.GetComponent<PlayerBullet>().direction = direction.normalized;
-            
         }
     }
 }
--- Assets/Scripts/ScriptsJamil/JugadorMovimiento.cs
+++ Assets/Scripts/ScriptsJamil/JugadorMovimiento.cs
@@ -7,12 +7,12 @@ public class JugadorMovimiento : MonoBehaviour
     Rigidbody2D rb2DMike;
     public float rapidez=2;
     Vector2 dirección;
-    Animator animaciónMike;
+    //Animator animaciónMike;
 
     void Start()
     {
         rb2DMike = GetComponent<Rigidbody2D>();
-        animaciónMike = GetComponent<Animator>();   
+        //animaciónMike = GetComponent<Animator>();   
     }
 
     // Update is called once per frame
@@ -27,9 +27,9 @@ public class JugadorMovimiento : MonoBehaviour
         float yInput = Input.GetAxisRaw("Vertical");
         dirección = new Vector2(xInput, yInput).normalized;
        
-        animaciónMike.SetFloat("Horizontal", xInput);
-        animaciónMike.SetFloat("Vertical", yInput);
-        animaciónMike.SetFloat("Rapidez",dirección.sqrMagnitude);
+        //animaciónMike.SetFloat("Horizontal", xInput);
+        //animaciónMike.SetFloat("Vertical", yInput);
+        //animaciónMike.SetFloat("Rapidez",dirección.sqrMagnitude);
 
         rb2DMike.velocity = new Vector2(xInput, yInput).normalized * rapidez;   
 
