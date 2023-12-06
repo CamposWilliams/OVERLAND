a1ac05a88e89d403282e4b135c5ce255b6ee4754
Jamil Cerrón <luis.cerron.2010@gmail.com>
2023-12-06 01:42:08 -0500

    Se modifico y actualizo el movimiento de los enemigos corresponidentes a las tareas PE29,PE30,PE35,PE36,PE42,PE43

--- Assets/Scripts/PERSONAJES/BONK/SeguirAstar.cs
+++ Assets/Scripts/PERSONAJES/BONK/SeguirAstar.cs
@@ -14,16 +14,15 @@ public class SeguirAstar : MonoBehaviour
     float valorDelParametroY;
     public Collider2D enemigoCollider;
     public bool reactivar;
-    float time;
-    Rigidbody2D enemigoRb2D;
+    public bool seHanJuntado;
+    public float time;
 
 
     void Start()
     {
         aiPath = GetComponent<AIPath>();
         EnemigoAnimacion = GetComponent<Animator>();
-        //StartCoroutine(VolverTrigger());
-        enemigoRb2D = GetComponent<Rigidbody2D>();    
+
     }
 
 
@@ -33,34 +32,14 @@ public class SeguirAstar : MonoBehaviour
         SeguimientoDeAnimacion();
         AtaqueAnimacion();
        
+        ReactivarCollider();
 
-        //if (reactivar == true)
-        //{
-        //    StartCoroutine(ReactivarCollider());
-        //}
-       
-
-
+        Debug.Log(seHanJuntado);
+        Debug.Log(time);
 
     }
 
-    IEnumerator VolverTrigger()
-    {
-        while (true)
-        {
-            if (enemigoRb2D.velocity == Vector2.zero)
-            {
-                enemigoCollider.isTrigger = true;
-                yield return new WaitForSeconds(3);           
-                enemigoCollider.isTrigger = false;
-            }     
-            else
-            {
-                yield return new WaitForSeconds(1); 
-            }
 
-        }
-    }
 
     void AtaqueAnimacion()
     {
@@ -124,15 +103,11 @@ public class SeguirAstar : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemigo1"))
         {
             enemigoCollider.isTrigger = true;
-           
+            //reactivar = true;
 
-            if (EnemigoAnimacion.GetBool("SeMueve") == false)
-            {
-                enemigoCollider.isTrigger = false;
-
-            }
+            
         }
-        
+
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
@@ -140,45 +115,44 @@ public class SeguirAstar : MonoBehaviour
         {
             enemigoCollider.isTrigger = true;
         }
-        if (collision.gameObject.CompareTag("Enemigo1"))
-        {
-            time += Time.deltaTime;
-            if (time >= 3)
-            {
-                enemigoCollider.isTrigger = true;
-
-                if (time >= 5)
-                {
-                 enemigoCollider.isTrigger = false;
-                }
-            }
-   
-
-            if (EnemigoAnimacion.GetBool("SeMueve") == false)
-            {
-                enemigoCollider.isTrigger = false;
-
-            }
-        }
-
 
     }
-    private void OnTriggerStay2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player")) 
+            {
+               enemigoCollider.isTrigger= false;
+            }
+
+        if (collision.CompareTag("Enemigo1"))
+        {
+            enemigoCollider.isTrigger= true;
+            seHanJuntado = true;
+             
+        }
+    }
+
+
+    void ReactivarCollider()
+    {
+          
+        if (EnemigoAnimacion.GetBool("SeMueve") == false)
         {
             enemigoCollider.isTrigger = false;
         }
 
+        if(seHanJuntado == true)
+        {
+            time += Time.deltaTime;
+
+            if (time >= 2)
+            {
+                enemigoCollider.isTrigger = false;
+                seHanJuntado =false;
+                time = 0;
+            }
+        }
+
     }
-
-    //IEnumerator ReactivarCollider()
-    //{
-    //    yield return null;
-    //    if (EnemigoAnimacion.GetBool("SeMueve") == false )
-    //    {
-    //        enemigoCollider.isTrigger = false;
-    //    }
-
-    //}
 }
