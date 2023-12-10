b06735266b06a2f43f7b6217e1e9b34d011286bb
José <jdavido2005@outlook.com>
2023-12-09 20:59:26 -0500

    JoseOlivera:M_10, PIU_07, PIU_06

---
+++ Assets/Scripts/MenuDeOpciones/VolumenControlador.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumenControlador : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider soundSlider;
+
+  
+    public string musicVolumeParameter = "Soundtracks";
+    public string soundVolumeParameter = "Sonidos";
+
+    void Start()
+    {
+       
+        musicSlider.value = PlayerPrefs.GetFloat(musicVolumeParameter, 1f);
+        soundSlider.value = PlayerPrefs.GetFloat(soundVolumeParameter, 1f);
+
+      
+        SetMusicVolume(musicSlider.value);
+        SetSoundVolume(soundSlider.value);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        Debug.Log("" + volume);
+        audioMixer.SetFloat(musicVolumeParameter, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(musicVolumeParameter, volume);
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        Debug.Log("" + volume);
+        audioMixer.SetFloat(soundVolumeParameter, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(soundVolumeParameter, volume);
+    }
+  
+}
